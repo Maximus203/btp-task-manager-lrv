b686a331c6625369c2b1857b07847949430b848e
Codoujuff <dioufmamecodou519@gmail.com>
2024-07-08 15:46:52 +0000

    Formulaire de creation d'utilisateur

--- resources/views/livewire/layout/projets/creer.blade.php
+++ resources/views/livewire/layout/projets/creer.blade.php
@@ -1,9 +1,21 @@
-<div>
-    <form class="max-w-md mx-auto mt-9" wire:submit.prevent="submit">
-        <div class="w-full mb-5 group">
+<div class="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-lg mt-10">
+    <form class="space-y-6" wire:submit.prevent="submit">
+        {{-- <div class="w-full mb-5 group">
             <x-input wire:model.blur="nomProjet" label="Nom du Projet" required placeholder="Codou Diouf" />
+        </div> --}}
+    <div>
+        <div class="flex items-center">
+            <label for="nomProjet" class="block text-sm font-medium text-gray-900 dark:text-white">Nom du projet</label>
         </div>
-        <div class="w-full mb-5 group">
+            <input type="text" name="nomProjet" id="nomProjet" wire:model.blur="nomProjet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Saisir le nom du projet" required />
+            @if ($errors->get("nomProjet"))
+              @foreach ($errors->get("nomProjet") as $error)
+                 <span class="text-red-500 text-sm">{{$error}}</span>
+              @endforeach
+            @endif
+    </div>
+
+        {{-- <div class="w-full mb-5 group">
             <div class="block mb-2">
                 <label for="client" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionner un client</label>
             </div>
@@ -13,7 +25,26 @@
                     <option value="{{ $client->id }}">{{ $client->nomcomplet }}</option>
                 @endforeach
             </select>
+        </div> --}}
+
+        <div>
+            <div class="flex items-center">
+              
+                <label for="underline_select" class="block text-sm font-medium text-gray-900 dark:text-white">Sélectionner un role</label>
+            </div>
+            <select id="underline_select" wire:model="idRole" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>Sélectionner un role</option>
+                <option value="1">Chef de projet</option>
+                <option value="2">Ouvrier</option>
+                <option value="3">Client</option>
+            </select>
+            @if ($errors->get("idRole"))
+            @foreach ($errors->get("idRole") as $error)
+                  <span class="text-red-500 text-sm">{{$error}}</span>
+            @endforeach
+          @endif
         </div>
+
 
         <div class="w-full mb-5 group">
             <div class="block mb-2">
@@ -41,9 +72,20 @@
             <button type="button" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" wire:click="addOuvrier">Ajouter un ouvrier</button>
         </div>
 
-        <div class="w-full mb-5 group">
+        {{-- <div class="w-full mb-5 group">
             <x-input wire:model.blur="budget" label="Budget" required placeholder="1200000" />
+        </div> --}}
+    <div>
+        <div class="flex items-center">
+            <label for="Budget" class="block text-sm font-medium text-gray-900 dark:text-white">Budget</label>
         </div>
+            <input type="numeric" name="Budget" id="Budget" wire:model.blur="budget" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Saisir le budget du projet" required />
+            @if ($errors->get("Budget"))
+              @foreach ($errors->get("Budget") as $error)
+                 <span class="text-red-500 text-sm">{{$error}}</span>
+              @endforeach
+            @endif
+    </div>
 
         <div class="w-full mb-5 group flex flex-row">
             <label for="statut" class="block w-1/4 mb-2 text-sm font-medium text-gray-500">Status</label>
@@ -72,6 +114,7 @@
         <div class="w-full mb-5 group">
             <x-button label="Ajouter" right-icon="check" outline interaction="primary" />
         </div>
+        
     </form>
 </div>
 <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
--- resources/views/livewire/utilisateur/creer.blade.php
+++ resources/views/livewire/utilisateur/creer.blade.php
@@ -1,32 +1,36 @@
-<div>
-    <form class="max-w-md mx-auto mt-6" wire:submit="register">
-        <div class="grid md:grid-cols-2 md:gap-6">
-            <div class="relative z-0 w-full mb-5 group">
-                <input type="text" name="prenom" id="prenom" wire:model="prenom" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="prenom" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Prénom</label>
+<div class="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-lg mt-10">
+    <form class="space-y-6" wire:submit="register">
+        <div>
+            <div class="flex items-center">
+              
+                <label for="prenom" class="block text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
+            </div>
+            <input type="text" name="prenom" id="prenom" wire:model="prenom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" " required />
             @if ($errors->get("prenom"))
               @foreach ($errors->get("prenom") as $error)
                     <span class="text-red-500 text-sm">{{$error}}</span>
-              @endforeach  
+              @endforeach
             @endif
+        </div>
+        <div>
+            <div class="flex items-center">
+              
+                <label for="nom" class="block text-sm font-medium text-gray-900 dark:text-white">Nom</label>
             </div>
-                <div class="relative z-0 w-full mb-5 group">
-                <input type="text" name="nom" id="nom" wire:model="nom" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="nom" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom</label>
-                @if ($errors->get("nom"))
+            <input type="text" name="nom" id="nom" wire:model="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" " required />
+            @if ($errors->get("nom"))
                 @foreach ($errors->get("nom") as $error)
                       <span class="text-red-500 text-sm">{{$error}}</span>
-                @endforeach  
+                @endforeach
               @endif
+        </div>
+        <div>
+            <div class="flex items-center">
+              
+                <label for="underline_select" class="block text-sm font-medium text-gray-900 dark:text-white">Sélectionner un role</label>
             </div>
-          </div>
-            
-          <div class="relative z-0 w-full mb-5 group">
-            <label for="underline_select" class="sr-only">Underline select</label>
-            
-            <select id="underline_select" wire:model="idRole"  required  class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+            <select id="underline_select" wire:model="idRole" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option selected>Sélectionner un role</option>
-                {{-- <option value="0">Directeur</option> --}}
                 <option value="1">Chef de projet</option>
                 <option value="2">Ouvrier</option>
                 <option value="3">Client</option>
@@ -34,21 +38,23 @@
             @if ($errors->get("idRole"))
             @foreach ($errors->get("idRole") as $error)
                   <span class="text-red-500 text-sm">{{$error}}</span>
-            @endforeach  
+            @endforeach
           @endif
-          </div>
-        <div class="relative z-0 w-full mb-5 group">
-            <input type="email" name="floating_email"  wire:model="email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email </label>
+        </div>
+        <div>
+            <div class="flex items-center">
+               
+                <label for="floating_email" class="block text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            </div>
+            <input type="email" name="floating_email" wire:model="email" id="floating_email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=" " required />
             @if ($errors->get("email"))
             @foreach ($errors->get("email") as $error)
                   <span class="text-red-500 text-sm">{{$error}}</span>
-            @endforeach  
+            @endforeach
           @endif
         </div>
-
-        <div class="w-full mb-5">
-        <input type="submit" value="Envoyer" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <div>
+            <input type="submit" value="Envoyer" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         </div>
     </form>
 </div>
