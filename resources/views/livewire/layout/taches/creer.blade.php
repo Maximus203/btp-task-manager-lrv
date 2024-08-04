5804290f8ae98b225a9dc8c60bee2360c86ab0a4
Codoujuff <dioufmamecodou519@gmail.com>
2024-08-04 23:32:37 +0000

    fin version4

--- resources/views/livewire/layout/taches/creer.blade.php
+++ resources/views/livewire/layout/taches/creer.blade.php
@@ -10,6 +10,7 @@
                 @error('nomTache') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
             </div>
 
+
             {{-- <div>
                 <label for="idProjet" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Projet</label>
                 <select id="idProjet" wire:model="idProjet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -20,12 +21,11 @@
                 </select>
                 @error('idProjet') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
             </div> --}}
-
-<div>
-    <label for="idProjet" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Projet</label>
-    <input type="hidden" wire:model="idProjet" value="{{ $idProjet }}" />
-    @error('idProjet') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
-</div>
+            {{-- <div>
+                <label for="idProjet" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Projet</label>
+                <input type="hidden" wire:model="idProjet" value="{{ $idProjet }}" />
+                @error('idProjet') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
+            </div> --}}
 
              <div>
                 <label for="ouvrier" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Sélectionner un ouvrier</label>
@@ -37,13 +37,11 @@
                 </select>
                 @error('ouvrier') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
             </div>
-
             <div>
                 <label for="description" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Description</label>
                 <input type="text" name="description" id="description" wire:model.blur="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Description" required />
                 @error('description') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
             </div>
-
             <div>
                 <label for="budget" class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Budget</label>
                 <input type="number" name="budget" id="budget" wire:model.blur="budget" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Budget" required />
@@ -71,7 +69,7 @@
                 <label class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Date de Début</label>
                 <input wire:model="dateDeDebut" type="date" name="dateDeDebut" id="dateDeDebut" placeholder="Date de début" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 @error('dateDeDebut') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
-<br>
+            <br>
                 <label class="block text-sm font-medium text-gray-900 dark:text-white mb-2">Date de Fin</label>
                 <input wire:model="dateDeFin" type="date" name="dateDeFin" id="dateDeFin" placeholder="Date de fin" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                 @error('dateDeFin') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
